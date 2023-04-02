e4c549186f184897c7fbee69be4fc8a7a1f6f38e
Muhammet Ali KARAKURT <karakurt3656@gmail.com>
2023-04-02 12:08:49 +0300

    Classes

--- Methods2/src/Main.java
+++ Methods2/src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
         String toplam = String.valueOf(topla(5,7));
         System.out.println("Toplamlari --> " + toplam );
+        int toplam1 = topla2(2,3,4,5,6,10);
+        System.out.println(toplam1);
 
     }
     public static void add(){   // void methodlari istediginiz operasyonlari yapar.
@@ -22,6 +24,14 @@ public class Main {
 
     public static int topla(int sayi1 , int sayi2){  // return donduren methods lar ..
         int toplam = sayi1 + sayi2 ;
+        return toplam;
+    }
+
+    public static int topla2(int... sayilar){
+        int toplam = 0 ;
+        for(int sayi : sayilar){
+            toplam += sayi;
+        }
         return toplam;
     }
 }
